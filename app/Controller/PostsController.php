a8c4e09607222f8114598254f50fc672ce44f8f4
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-09-12 05:45:49 +0900

    Add View action for Posts
    ```
    $ vim app/Controller/PostsController.php
    $ vim app/View/Posts/view.haml
    ```

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -5,12 +5,20 @@ App::uses('AppController', 'Controller');
  *
  */
 class PostsController extends AppController {
-
-/**
- * Scaffold
- *
- * @var mixed
- */
-	public $scaffold;
-
+  public function view($id) {
+    if(isset($id)){
+      // $id に該当するフィールドを取得するための条件
+      $conditions = array( "Post.id"  => $id);
+    } else {
+      $conditions = "";
+    }
+    // 検索結果を id 昇順で取得
+    $order = array('Post.id asc');  // Post テーブル内の id が昇順となるように
+    // Posts テーブルから検索する
+    $post = $this->Post->find('first', array(
+      'conditions' => $conditions,   // 検索条件の指定
+      'order' => $order,                  // 取得順序の指定
+    ));
+    $this->set('post', $post);  // View(app/View/view.ctp)に $posts 変数を渡す
+  }
 }
